81ed5a438e098696fe77f4f22064da76d1e41612
Gordon Cassie <gordoncassie@gmail.com>
2025-05-17 08:31:41 -0700

    hello world of loading a pdf

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,33 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const assert = std.debug.assert;
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const check = gpa.deinit();
+        if (check == .leak) @panic("Memory leak detected!");
+    }
+    const allocator = gpa.allocator();
+    // realpathAlloc resolves all symbolic links and normalizes the path by removing any
+    // "." and ".." components. It returns the absolute canonical path.
+    const normalized_path = try std.fs.cwd().realpathAlloc(allocator, "./test/text-test.pdf");
+    defer allocator.free(normalized_path);
+
+    // Convert to C-compatible string (UTF-8, null-terminated)
+    const c_path = try allocator.dupeZ(u8, normalized_path);
+    defer allocator.free(c_path);
+    std.debug.print("Normalized path: {s}\n", .{c_path});
+
+    try lib.bindPdfium();
+    lib.FPDF_InitLibrary();
+    const pdf = lib.FPDF_LoadDocument(c_path, null) orelse {
+        std.debug.print("LoadDocument failed with error code: {?}", .{lib.FPDF_GetLastError()});
+        return;
+    };
+    const page_count = lib.FPDF_GetPageCount(pdf);
+
+    std.debug.print("Page count: {}\n", .{page_count});
 
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,23 @@
 const std = @import("std");
 const testing = std.testing;
 
+pub var c_pdfium: ?std.DynLib = null;
+
+pub const FPDFDocument = *anyopaque;
+
+pub var FPDF_LoadDocument: *const fn ([*c]const u8, [*c]u8) callconv(.C) ?FPDFDocument = undefined;
+pub var FPDF_InitLibrary: *const fn () callconv(.C) void = undefined;
+pub var FPDF_GetPageCount: *const fn (FPDFDocument) callconv(.C) c_int = undefined;
+pub var FPDF_GetLastError: *const fn () callconv(.C) c_int = undefined;
+
+pub fn bindPdfium() !void {
+    c_pdfium = try std.DynLib.open("./vendor/pdfium-mac-arm64/lib/libpdfium.dylib");
+    FPDF_InitLibrary = c_pdfium.?.lookup(@TypeOf(FPDF_InitLibrary), "FPDF_InitLibrary").?;
+    FPDF_LoadDocument = c_pdfium.?.lookup(@TypeOf(FPDF_LoadDocument), "FPDF_LoadDocument").?;
+    FPDF_GetPageCount = c_pdfium.?.lookup(@TypeOf(FPDF_GetPageCount), "FPDF_GetPageCount").?;
+    FPDF_GetLastError = c_pdfium.?.lookup(@TypeOf(FPDF_GetLastError), "FPDF_GetLastError").?;
+}
+
 pub export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
