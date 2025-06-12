4f62be3a7e55f3b01488b7c6837c2b44b5d455ae
Gordon Cassie <gordoncassie@gmail.com>
2025-06-12 11:54:26 -0700

    Cleanup build.zig and make it only import zig-img for testing

--- build.zig
+++ build.zig
@@ -33,62 +33,32 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
     lib_mod.addIncludePath(b.path("vendor/pdfium-mac-arm64/include"));
 
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
+    // Declare a separate module for testing so we can include the zigimg dependency
+    // without it being part of the main library
+    const lib_mod_test = b.addModule("zig-pdfium-test", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const dep_zigimg = b.dependency("zigimg", .{});
+    lib_mod_test.addImport("zigimg", dep_zigimg.module("zigimg"));
+    lib_mod_test.addIncludePath(b.path("vendor/pdfium-mac-arm64/include"));
+
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "zig_pdfium",
         .root_module = lib_mod,
     });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 
-    // Create an executable
-    const exe = b.addExecutable(.{
-        .name = "pdf-splitter",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
-    exe.root_module.addImport("zig-pdfium", lib_mod);
-    exe.linkLibC();
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+        .root_module = lib_mod_test,
         .test_runner = .{ .path = b.path("src/test_runner.zig"), .mode = .simple },
     });
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const dep_zigimg = b.dependency("zigimg", .{});
-    lib_unit_tests.root_module.addImport("zigimg", dep_zigimg.module("zigimg"));
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-
-    // Add a run step for the executable
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the pdf-splitter");
-    run_step.dependOn(&run_cmd.step);
 }
